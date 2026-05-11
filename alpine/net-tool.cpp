d64b0ddffa7827b3bf69553aa14934dcce4e3bce
coffee <alpine@coffee.lan>
2026-05-11 20:12:34 +0500

    fixed net-tool

--- alpine/net-tool.cpp
+++ alpine/net-tool.cpp
@@ -9,14 +9,53 @@ int main () {
   std::string mkwith;
 
   
-  std::cout << "!!! WARNING !!! Run as root\n \n \n";
   std::cout << "What interface do you want turn off/turn on? (basicly eth1/eth0/wlan0) ";
   std::getline(std::cin, interface);
   std::cout << "What you make with " << interface << " (down or up) ";
   std::getline(std::cin, mkwith);
 
 
-  std::string cmd = "ip link set " + interface + " " + mkwith;
-  int rc = std::system(cmd.c_str());
-  return rc == -1 ? 1 : 0;
+  if (interface == "eth0") {
+    if (mkwith == "up") {
+      system("doas ip link set eth0 up");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else if (mkwith == "down") {
+      system("doas ip link set eth0 down");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else {
+      std::cerr;
+      return 1;
+    }
+  } else if (interface == "eth1") {
+     if (mkwith == "up") {
+      system("doas ip link set eth1 up");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else if (mkwith == "down") {
+      system("doas ip link set eth1 down");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else {
+      std::cerr;
+      return 1;
+    } 
+
+  } else if (interface == "wlan0") {
+    if (mkwith == "up") {
+      system("doas ip link set wlan0 up");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else if (mkwith == "down") {
+      system("doas ip link set wlan0 down");
+      std::cout << "Succeful!\n";
+      return 0;
+    } else {
+      std::cerr;
+      return 1;
+    }
+  } else {
+    std::cerr;
+  }
 }
