2f80f8730d78dbe18b9960c512b9711a472b1a31
coffee <alpine@coffee.lan>
2026-05-14 19:19:44 +0500

    Add my apk repos and cahnged autoinstall.sh !!!

--- alpine/auto_install.sh
+++ alpine/auto_install.sh
@@ -9,14 +9,24 @@ if [ "$(id -u)" -eq 0 ]; then
   exit 1
 fi
 
-# apk
-doas apk add neovim ripgrep fd bash fish flatpak docker lazygit lazydocker \
-  doas sway waybar kitty wofi kitty-kitten imagemagick \
-  build-base cmake g++ gcc mpv gtkmm4 gtkmm4-dev rofi udiskie udisks2 \
-  pipewire wireplumber pipewire-pulse pipewire-jack pipewire-alsa pavucontrol pulseaudio \
-  xdg-desktop-portal-wlr \
-  bluez bluez-openrc pipewire-spa-bluez blueman \
-  qt6-qtsvg qt6-qtbase qt6-qtvirtualkeyboard qt6-qtmultimedia qt6-qtmultimedia-ffmpeg
+# Apk
+# Base
+doas apk add neovim ripgrep fd bash fish flatpak docker lazygit lazydocker
+# Term + WM + WM Depencies
+doas sway waybar kitty wofi kitty-kitten imagemagick waybar
+# Coding/extra
+build-base cmake g++ gcc mpv gtkmm4 gtkmm4-dev rofi udiskie udisks2
+# Sound
+pipewire wireplumber pipewire-pulse pipewire-jack pipewire-alsa pavucontrol pulseaudio
+# Docker
+xdg-desktop-portal-wlr
+# Bluetooth
+bluez bluez-openrc pipewire-spa-bluez blueman
+# Silent SDDM
+qt6-qtsvg qt6-qtbase qt6-qtvirtualkeyboard qt6-qtmultimedia qt6-qtmultimedia-ffmpeg
+# VM
+virt-manager qemu qemu-img qemu-x86_64 qemu-storage-daemon qemu-ui-gtk libvirt libvirt-daemon libvirt-daemon-openrc \
+  libvirt-qemu libvirt-common-drivers
 
 # Openrc service
 # Pipewire / Wirepliumber
@@ -29,17 +39,26 @@ doas rc-update add sddm
 # Bluetooth
 doas rc-update add bluetooth
 #doas rc-service bluetooth start
+# Libvirt
+doas rc-update add libvirtd
+#doas reboot
 
 # Flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak install -y --user com.obsproject.Studio dev.vencord.Vesktop md.obsidian.Obsidian \
   org.keepassxc.KeePassXC com.valvesoftware.Steam
-flatpak install -y --system app.zen_browser.Zen
+flatpak install -y --system app.zen_browser.Zen \
+  net.sonobux.SonoBus io.github.dimtpap.coppwr io.github.Soundux com.github.wwmm.easyeffects
 
-# Docker service
+# Groups
+#   Docker
 doas addgroup docker 2>/dev/null || true
-doas adduser "$(whoami)" docker
+doas addgroup "$(whoami)" docker
 
 doas addgroup input 2>/dev/null || true
-doas adduser "$(whoami)" input
+doas addgroup "$(whoami)" input
+
+#   KVM
+doas addgroup kvm || true
+doas addgroup "$(whoami)" kvm
